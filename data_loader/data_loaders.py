ae13f31fecac9711a8ab39a565864401f5f8b81c
aryanshomray <aryansh.omray@outlook.com>
2020-12-22 15:34:30 +0530

    Initial Commit

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,38 @@
-from torchvision import datasets, transforms
+from torch.utils.data.dataset import T_co
+from torchvision import transforms
 from base import BaseDataLoader
-
-
+from torch.utils.data import Dataset
+import pandas as pd
+import rawpy
 class MnistDataLoader(BaseDataLoader):
     """
-    MNIST data loading demo using BaseDataLoader
+    Data loading demo using BaseDataLoader
     """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
+    def __init__(self, data_dir, batch_size, noise, shuffle=True, validation_split=0.0, num_workers=1, crop_size=256):
         self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
+        self.dataset = ImageDataset(crop_size, noise)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class ImageDataset(Dataset):
+
+    def __init__(self, crop_size:int, noise:str):
+        super().__init__()
+        self.trsfms = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.RandomCrop(crop_size)
+        ])
+
+        self.train_list:pd.DataFrame = pd.read_csv()
+        self.val_list:pd.DataFrame = pd.read_csv()
+        self.main_list:pd.DataFrame = pd.concat([self.train_list, self.val_list], axis=0)
+        self.noise = noise
+    def __len__(self) -> int:
+        return self.train_list.shape[0] + self.val_list.shape[0]
+
+    def __getitem__(self, index) -> T_co:
+        clean = rawpy.imread(self.main_list[index][0]).postprocess()
+        noisy = rawpy.imread(self.main_list[index][1]).postprocess()
+        clean = self.trsfms(clean)
+        noisy = self.trsfms(noisy)
+        return clean, noisy
--- model/metric.py
+++ model/metric.py
@@ -1,20 +1,20 @@
 import torch
+from .metric_classes import PSNR, SSIM
+import lpips
 
+_psnr = PSNR()
+_ssim = SSIM()
+_lpips = lpips.LPIPS(net="vgg")
 
-def accuracy(output, target):
+def psnr(output, target):
     with torch.no_grad():
-        pred = torch.argmax(output, dim=1)
-        assert pred.shape[0] == len(target)
-        correct = 0
-        correct += torch.sum(pred == target).item()
-    return correct / len(target)
+        return _psnr(output, target)
 
 
-def top_k_acc(output, target, k=3):
+def ssim(output, target):
     with torch.no_grad():
-        pred = torch.topk(output, k, dim=1)[1]
-        assert pred.shape[0] == len(target)
-        correct = 0
-        for i in range(k):
-            correct += torch.sum(pred[:, i] == target).item()
-    return correct / len(target)
+        return _ssim(output, target)
+
+def lpips_(output, target):
+    with torch.no_grad():
+        return _lpips(2*output-1, 2*target-1)
