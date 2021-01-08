48e3e524d6aeeed5db11a7637e9812a370a010dc
aryanshomray <aryansh.omray@outlook.com>
2021-01-08 14:07:10 +0530

    Improved Loss Function

--- model/loss.py
+++ model/loss.py
@@ -6,7 +6,7 @@ log2pi = np.log(2 * np.pi)
 
 def calc_prior(output: torch.tensor):
     prior = -0.5 * ((output ** 2) + torch.tensor(log2pi))
-    prior = torch.sum(prior, dim=[1,2,3])
+    prior = torch.sum(prior, dim=[1, 2, 3])
     return prior
 
 
