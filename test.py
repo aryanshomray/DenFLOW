342a3eff4e841591b46a5ccd9dff654fd6f0aff0
aryanshomray <aryansh.omray@outlook.com>
2021-01-25 13:36:54 +0530

    Added Testing Code

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+from datetime import datetime
+import numpy as np
 import torch
 from tqdm import tqdm
 import data_loader.data_loaders as module_data
@@ -6,6 +9,21 @@ import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
+from PIL import Image
+
+
+def concat_images(imga, imgb):
+    """
+    Combines two color image ndarrays side-by-side.
+    """
+    ha, wa = imga.shape[:2]
+    hb, wb = imgb.shape[:2]
+    max_height = np.max([ha, hb])
+    total_width = wa + wb
+    new_img = np.zeros(shape=(max_height, total_width, 3))
+    new_img[:ha, :wa] = imga
+    new_img[:hb, wa:wa + wb] = imgb
+    return new_img
 
 
 def main(config):
@@ -14,11 +32,12 @@ def main(config):
     # setup data_loader instances
     data_loader = getattr(module_data, config['data_loader']['type'])(
         config['data_loader']['args']['data_dir'],
-        batch_size=512,
+        batch_size=1,
         shuffle=False,
         validation_split=0.0,
-        training=False,
-        num_workers=2
+        num_workers=2,
+        process_dataset=False,
+        crop_size=256
     )
 
     # build model architecture
@@ -30,7 +49,7 @@ def main(config):
     metric_fns = [getattr(module_metric, met) for met in config['metrics']]
 
     logger.info('Loading checkpoint: {} ...'.format(config.resume))
-    checkpoint = torch.load(config.resume)
+    checkpoint = torch.load('config.resume', map_location=torch.device('cpu'))
     state_dict = checkpoint['state_dict']
     if config['n_gpu'] > 1:
         model = torch.nn.DataParallel(model)
@@ -43,22 +62,39 @@ def main(config):
 
     total_loss = 0.0
     total_metrics = torch.zeros(len(metric_fns))
-
+    try:
+        os.mkdir('Results')
+    except:
+        pass
+    PATH = f'Results/{datetime.now()}'
+    os.mkdir(PATH)
+    img_counter = 0
     with torch.no_grad():
-        for i, (data, target) in enumerate(tqdm(data_loader)):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
+        for i, (clean, noisy) in enumerate(tqdm(data_loader)):
+            Batch_Size = clean.shape[0]
+            clean, noisy = clean.to(device), noisy.to(device)
+            latent_space = torch.normal(0.0, 0.0, clean.shape).to(device)
+            output = model(latent_space, noisy, reverse=True)[0]
+            out = output.cpu().permute([0, 2, 3, 1]).numpy()
+
+            clean_out = clean.cpu().permute([0, 2, 3, 1]).numpy()
+            for j in range(Batch_Size):
+                out_img = out[j]
+                clean_img = clean_out[j]
+                save_img = Image.fromarray(concat_images(out_img, clean_img))
+                save_img.save(f'{PATH}/{img_counter}.png')
+                img_counter += 1
 
             #
             # save sample images, or do something with output here
             #
 
             # computing loss, metrics on test set
-            loss = loss_fn(output, target)
-            batch_size = data.shape[0]
+            loss = loss_fn(output, clean)
+            batch_size = clean.shape[0]
             total_loss += loss.item() * batch_size
             for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
+                total_metrics[i] += metric(output, clean) * batch_size
 
     n_samples = len(data_loader.sampler)
     log = {'loss': total_loss / n_samples}
